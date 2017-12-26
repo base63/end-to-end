735326e87c3b5ec1936332347f86707b97ff83ea
Horia Coman <horia141@gmail.com>
2017-12-27 00:59:51 +0200

    Initial commit
    Have a dumb .env for now

---
+++ README.md
@@ -0,0 +1,3 @@
+# End-To-End Tests [![Build Status](https://travis-ci.org/base63/end-to-end.svg?branch=master)](https://travis-ci.org/base63/end-to-end) [![Coverage](https://codecov.io/gh/base63/end-to-end/branch/master/graph/badge.svg)](https://codecov.io/gh/base63/end-to-end)
+
+End-to-end tests of the whole base63.
---
+++ src/index.js
@@ -0,0 +1,56 @@
+describe('base63fe', function () {
+    describe('home page', function() {
+        it('contains text', function() {
+            cy.visit('http://localhost:10003/');
+            cy.contains('Home page');
+        });
+
+        it('links to admin page', function() {
+            cy.visit('http://localhost:10003/');
+            cy.contains('Admin').click();
+            cy.contains('Admin page');
+        });
+    });
+
+    describe('admin page', function() {
+        it('contains text', function() {
+            cy.visit('http://localhost:10003/admin');
+            cy.contains('Admin page');
+        });
+
+        it('links to home page', function() {
+            cy.visit('http://localhost:10003/admin');
+            cy.contains('Home').click();
+            cy.contains('Home page');
+        });
+    });
+});
+
+
+describe('blogfe', function () {
+    describe('home page', function() {
+        it('contains text', function() {
+            cy.visit('http://localhost:10004/');
+            cy.contains('Home page');
+        });
+
+        it('links to post page', function() {
+            cy.visit('http://localhost:10004/');
+            cy.contains('Post').click();
+            cy.contains('A post page');
+        });
+    });
+
+    describe('post page', function() {
+        it('contains text', function() {
+            cy.visit('http://localhost:10004/post');
+            cy.contains('A post page');
+        });
+
+        it('links to home page', function() {
+            cy.visit('http://localhost:10004/post');
+            cy.contains('Home').click();
+            cy.contains('Home page');
+        });
+    });
+});
